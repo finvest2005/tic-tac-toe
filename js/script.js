7e6c9376cfe9634b2513b404137ddc276fc8d56b
finvest2005 <finvest2005@yandex.ru>
2025-10-18 09:59:37 +0300

    Delete paragraph with game status text

--- js/script.js
+++ js/script.js
@@ -269,13 +269,10 @@ function tictactoeHTML() {
         self.state = 'playGame';
     }
     function playGame_escapeEntered(value) {
-        var elem;
         clearHTMLBoard();
         console.log('game over');
         container = document.querySelector('.main');
         container.style.visibility = 'hidden';
-        elem = document.querySelector('.gamestatus');
-        elem.innerText = 'game over !!!!';
         self.state = undefined;
     }
     function playGame_numberEntered(value) {
